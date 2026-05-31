a6ba3eefd88117760ac5471523f5f6cd68e54ca6
MAcDen34 <d.ngabo@alustudent.com>
2026-05-31 19:45:24 +0200

    Fix base_model init for kwargs, fix test_kwargs_one, add close() to FileStorage

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -77,8 +77,8 @@ class test_basemodel(unittest.TestCase):
     def test_kwargs_one(self):
         """ """
         n = {'Name': 'test'}
-        with self.assertRaises(KeyError):
-            new = self.value(**n)
+        new = self.value(**n)
+        self.assertIsNotNone(new)
 
     def test_id(self):
         """ """
