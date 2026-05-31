963422d12285531d8857964ff617b154c9b66275
MAcDen34 <d.ngabo@alustudent.com>
2026-05-31 21:55:06 +0200

    Update all() to support optional cls filter, delete() already present

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,13 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a directory of objects currently in storage"""
+        if cls is None:
+            return FileStorage.__objects
+        else:
+            return {k: v for k, v in FileStorage.__objects.items()
+                    if k.split('.')[0] == cls.__name__}
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
