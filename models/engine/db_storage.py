30fbd3afffd3b6725a953cdf0b81e41ba76c3e48
MAcDen34 <d.ngabo@alustudent.com>
2026-05-31 16:20:30 +0200

    Add DBStorage engine and MySQL test setup scripts

---
+++ models/engine/db_storage.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""This module defines the DBStorage engine"""
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
+
+class DBStorage:
+    """Database storage engine using SQLAlchemy"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Instantiate DBStorage"""
+        user = getenv('HBNB_MYSQL_USER')
+        pwd = getenv('HBNB_MYSQL_PWD')
+        host = getenv('HBNB_MYSQL_HOST', 'localhost')
+        db = getenv('HBNB_MYSQL_DB')
+        env = getenv('HBNB_ENV')
+
+        self.__engine = create_engine(
+            'mysql+mysqldb://{}:{}@{}/{}'.format(user, pwd, host, db),
+            pool_pre_ping=True
+        )
+
+        if env == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Query all objects of a class"""
+        classes = [User, State, City, Amenity, Place, Review]
+        result = {}
+        if cls:
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = "{}.{}".format(type(obj).__name__, obj.id)
+                result[key] = obj
+        else:
+            for c in classes:
+                objs = self.__session.query(c).all()
+                for obj in objs:
+                    key = "{}.{}".format(type(obj).__name__, obj.id)
+                    result[key] = obj
+        return result
+
+    def new(self, obj):
+        """Add object to current session"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Commit all changes to current session"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """Delete object from current session"""
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """Reload data from database"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(
+            bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def close(self):
+        """Close the session"""
+        self.__session.close()
---
+++ tests/test_console.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+"""Unittest for console.py"""
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
+
+
+class TestConsole(unittest.TestCase):
+    """Tests for the HBNB console"""
+
+    def test_prompt(self):
+        """Test prompt is correct"""
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    def test_empty_line(self):
+        """Test empty line does nothing"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("")
+            self.assertEqual("", f.getvalue())
+
+    def test_quit(self):
+        """Test quit command"""
+        with self.assertRaises(SystemExit):
+            HBNBCommand().onecmd("quit")
+
+    def test_help(self):
+        """Test help command exists"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("help")
+            self.assertIn("Documented commands", f.getvalue())
+
+    def test_show_no_args(self):
+        """Test show with no args"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show")
+            self.assertIn("** class name missing **", f.getvalue())
+
+    def test_create_no_args(self):
+        """Test create with no args"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create")
+            self.assertIn("** class name missing **", f.getvalue())
+
+    def test_destroy_no_args(self):
+        """Test destroy with no args"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("destroy")
+            self.assertIn("** class name missing **", f.getvalue())
+
+    def test_all_no_args(self):
+        """Test all with no args"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("all")
+            self.assertIsNotNone(f.getvalue())
+
+    def test_update_no_args(self):
+        """Test update with no args"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("update")
+            self.assertIn("** class name missing **", f.getvalue())
+
+    def test_invalid_class(self):
+        """Test show with invalid class"""
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("show MyModel")
+            self.assertIn("** class doesn't exist **", f.getvalue())
+
+
+if __name__ == "__main__":
+    unittest.main()
